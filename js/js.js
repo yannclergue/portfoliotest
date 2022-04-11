fc8ac33153c7939b4650e81726c51037051593bc
yannclergue <73096566+yannclergue@users.noreply.github.com>
2022-04-11 22:30:33 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,51 @@
+document.querySelector(".burger").addEventListener("click", menu)
+
+function menu(){
+    document.querySelector("nav").classList.toggle("viens")
+}
+document.querySelector(".burger2").addEventListener("click", menus)
+
+function menus(){
+    document.querySelector("nav").classList.remove("viens")
+}
+
+
+document.querySelector("#ecofood").addEventListener("click", eco)
+
+function eco(){
+    document.querySelector(".ecofood").classList.toggle("salut")
+    document.querySelector(".LTJV").classList.remove("salut")
+    document.querySelector(".nutry").classList.remove("salut")
+     document.querySelector(".youtube").classList.remove("salut")
+}
+document.querySelector("#LTJV").addEventListener("click", jeu)
+
+function jeu(){
+    document.querySelector(".LTJV").classList.toggle("salut")
+    document.querySelector(".ecofood").classList.remove("salut")
+    document.querySelector(".nutry").classList.remove("salut")
+     document.querySelector(".youtube").classList.remove("salut")
+}
+document.querySelector("#nutry").addEventListener("click", nutry)
+
+function nutry(){
+    document.querySelector(".LTJV").classList.remove("salut")
+    document.querySelector(".ecofood").classList.remove("salut")
+    document.querySelector(".nutry").classList.toggle("salut")
+     document.querySelector(".youtube").classList.remove("salut")
+}
+document.querySelector("#YT").addEventListener("click", youtube)
+
+function youtube(){
+    document.querySelector(".LTJV").classList.remove("salut")
+    document.querySelector(".ecofood").classList.remove("salut")
+    document.querySelector(".nutry").classList.remove("salut")
+    document.querySelector(".youtube").classList.toggle("salut")
+}
+
+
+document.querySelectorAll("h3").forEach(lien => {
+    lien.addEventListener("click", function(){
+        document.querySelector("nav").classList.toggle("viens")
+    })
+})
